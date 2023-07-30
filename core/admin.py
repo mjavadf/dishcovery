9f05d22e57c9055c11de691ca44ffe3e12f0eed3
Javad Farokhi <javad.me1376@gmail.com>
2023-07-30 14:47:16 +0200

    register user admin model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+# register user model
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_staff', 'date_joined']
