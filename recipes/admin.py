4c4581009934fa32556880d1f92d15e2fa93fef0
Javad Farokhi <javad.me1376@gmail.com>
2023-07-30 15:30:48 +0200

    register recipe and ingredient in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = models.RecipeIngredient
+    extra = 1
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'created_at', 'modified_at']
+    inlines = [RecipeIngredientInline]
+    
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at', 'modified_at']
