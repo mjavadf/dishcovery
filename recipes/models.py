0d507b183f8750a695ae8fb4eca7673b9764bab4
Javad Farokhi <javad.me1376@gmail.com>
2023-07-30 15:41:25 +0200

    add image field to models

--- recipes/models.py
+++ recipes/models.py
@@ -5,7 +5,7 @@ user = settings.AUTH_USER_MODEL
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    # image = models.ImageField(upload_to='images/ingredients/')
+    image = models.ImageField(upload_to='images/ingredients/')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(user, on_delete=models.SET_NULL, null=True, blank=True, related_name="+")
@@ -28,7 +28,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
     description = models.TextField()
-    # image = models.ImageField(upload_to='images/recipes/')
+    image = models.ImageField(upload_to='images/recipes/')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     
@@ -46,7 +46,7 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipes")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
     custom_name = models.CharField(max_length=255, blank=True, null=True)
-    # custom_image = models.ImageField(upload_to='images/ingredients/', blank=True, null=True)
+    custom_image = models.ImageField(upload_to='images/ingredients/', blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=255)
     
