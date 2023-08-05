a79751d8b0fa59f497a19cd6f3e9040ea2915f1d
Javad Farokhi <javad.me1376@gmail.com>
2023-08-05 20:44:08 +0200

    fix RecipeIngredient's Model

--- recipes/models.py
+++ recipes/models.py
@@ -50,8 +50,8 @@ class RecipeIngredient(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=255)
     
-    def str(self):
-        return f"{self.amount} {self.unit} of {self.ingredient.name}"
+    def __str__(self):
+        return f"{self.ingredient.name} ({self.amount} {self.unit})"
     
     class Meta:
         verbose_name = 'Recipe Ingredient'
