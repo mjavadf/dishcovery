ed179fa732c153ffcfe43838c0e4698d15314a03
Javad Farokhi <javad.me1376@gmail.com>
2023-08-07 14:23:46 +0200

    Update serialzier to show ingredients for each recipe

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class RecipeIngredientSimpleSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(method_name="get_name", read_only=True)
+    image = serializers.SerializerMethodField(method_name="get_image", read_only=True)
+    
+    def get_name(self, obj):
+        if obj.custom_name is not None:
+            return obj.custom_name
+        return obj.ingredient.name
+    
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.custom_image != "":
+            return request.build_absolute_uri(obj.custom_image.url)
+        return request.build_absolute_uri(obj.ingredient.image.url)
+    
+    class Meta:
+        model = RecipeIngredient
+        fields = ["name", "amount", "unit", "image"]
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = RecipeIngredientSimpleSerializer(many=True)
     class Meta:
         model = Recipe
         fields = [
--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,12 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Recipe, Ingredient
 from .serializers import RecipeSerializer
 
+
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["request"] = self.request
+        return context
