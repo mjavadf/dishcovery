3ba3ee5ebc3de7515dcd61610675e66a2cff24bb
Javad Farokhi <javad.me1376@gmail.com>
2023-08-08 13:17:33 +0200

    Change main urlpattern

--- dishcovery/urls.py
+++ dishcovery/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
-    path("recipes/", include("recipes.urls")),
+    path("", include("recipes.urls")),
 ]
 
 if settings.DEBUG:
